464a51a8b14e6743dd273a4e81e1310ff8ea4d27
Taariq Elliott <codebytaariq@gmail.com>
2025-09-04 20:52:03 -0400

    Sync

--- convex/schema.ts
+++ convex/schema.ts
@@ -5,20 +5,79 @@ import { v } from 'convex/values';
 export default defineSchema({
   ...authTables,
   users: defineTable({
-    name: v.optional(v.string()),
-    image: v.optional(v.string()),
-    email: v.optional(v.string()),
-    emailVerificationTime: v.optional(v.float64()),
-    phone: v.optional(v.string()),
-    phoneVerificationTime: v.optional(v.float64()),
-    isAnonymous: v.optional(v.boolean()),
-
-    githubId: v.optional(v.number()),
-  }).index('email', ['email']),
-
-  messages: defineTable({
-    author: v.string(),
-    content: v.string(),
-    timestamp: v.optional(v.number()),
-  }).index('by_author', ['author']),
+    role: v.string(),
+    email: v.string(),
+    username: v.string(),
+    firstName: v.string(),
+    lastName: v.string(),
+    schoolId: v.id('schools'),
+    organizationIds: v.optional(v.array(v.id('organizations'))),
+    profileImgUrl: v.optional(v.string()),
+    attendingEvents: v.optional(v.array(v.id('events'))),
+    maybeAttendingEvents: v.optional(v.array(v.id('events'))),
+  }),
+  schools: defineTable({
+    schoolName: v.string(),
+    county: v.optional(v.string()),
+    schoolType: v.optional(v.string()),
+    street: v.optional(v.string()),
+    city: v.optional(v.string()),
+    state: v.optional(v.string()),
+    zip: v.optional(v.string()),
+    events: v.optional(
+      v.array(
+        v.object({
+          eventName: v.string(),
+          eventDate: v.string(),
+        })
+      )
+    ),
+  }),
+  organizations: defineTable({
+    organizationName: v.string(),
+    description: v.optional(v.string()),
+    schoolId: v.id('schools'),
+    memberIds: v.optional(v.array(v.id('users'))),
+    orgImgUrl: v.optional(v.string()),
+    contactEmail: v.optional(v.string()),
+  }),
+  events: defineTable({
+    eventName: v.string(),
+    eventDate: v.string(),
+    eventTimeFrom: v.string(),
+    eventTimeTo: v.string(),
+    eventContactPerson: v.optional(v.string()),
+    eventContactEmail: v.optional(v.string()),
+    eventContactPhone: v.optional(v.string()),
+    eventOrganizationId: v.optional(v.id('organizations')),
+    eventSchoolId: v.id('schools'),
+    attendingCount: v.number(),
+    maybeAttendingCount: v.number(),
+    createdBy: v.id('users'),
+    eventTags: v.array(v.string()),
+    eventImgUrl: v.optional(v.string()),
+    eventShareUrl: v.optional(v.string()),
+  }),
+  requests: defineTable({
+    eventRequestName: v.string(),
+    eventRequestCreatedBy: v.id('users'),
+    eventRequestSchoolId: v.id('schools'),
+    eventRequestOrganizationId: v.optional(v.id('organizations')),
+    eventRequestTags: v.array(v.string()),
+    eventRequestLikeCount: v.number(),
+    likedByUsers: v.optional(v.array(v.id('users'))),
+    eventRequestContactEmail: v.optional(v.string()),
+    eventRequestMessages: v.optional(
+      v.array(
+        v.object({
+          authorId: v.id('users'),
+          message: v.string(),
+          timestamp: v.string(),
+        })
+      )
+    ),
+    createdAt: v.string(),
+    updatedAt: v.optional(v.string()),
+    status: v.optional(v.string()),
+  }),
 });
